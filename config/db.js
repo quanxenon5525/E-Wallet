0d78420cb6b19b61d97f107a7145d4c657749b34
Minh Quan <quanxenon5525@gmail.com>
2022-05-30 17:00:39 +0700

    updated

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose
 
-    .connect("mongodb://localhost:27017/auth", {
+    .connect(process.env.DATABASE_URL || "mongodb://localhost:27017/auth", {
         useUnifiedTopology: true,
         useNewUrlParser: true,
     }
