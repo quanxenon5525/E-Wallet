dddf011d890518e185bfb04491a6363c884a2395
Minh Quan <quanxenon5525@gmail.com>
2022-05-31 10:06:06 +0700

    fix dang ki

--- middlewares/transporter.js
+++ middlewares/transporter.js
@@ -2,7 +2,6 @@ const nodemailer = require("nodemailer")
 let transporter = nodemailer.createTransport({
     service: "gmail",
     host: 'smtp.gmail.com',
-    port: 465,
     secure: true,
     auth: {
         user: "test28150106@gmail.com",
